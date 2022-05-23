4301670b9fa5e5f446f5327f2bd2361307fa1052
egormozzharov <egormozzharov@gmail.com>
2022-05-23 20:03:15 +0300

    ERC1155 Contract and deployment script were added

---
+++ scripts/deployERC1155.ts
@@ -0,0 +1,15 @@
+import { ethers } from "hardhat";
+import { ContractFactory, Contract } from 'ethers';
+
+async function main() {
+  console.log("Deploying the contract...");
+  const contractFactory: ContractFactory = await ethers.getContractFactory("ERC1155Contract");
+  console.log("Contract factory");
+  const contract: Contract = await contractFactory.deploy({gasPrice: 70000000000});
+  await contract.deployed();
+  console.log("ERC1155Contract deployed to:", contract.address);
+}
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
