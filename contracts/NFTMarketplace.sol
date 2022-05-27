2bc1d73a0f334e0d5f7014c388e4fccca5c5ffb6
egormozzharov <egormozzharov@gmail.com>
2022-05-27 14:41:13 +0300

    Unused import was removed

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "./interfaces/IERC721Mintable.sol";
