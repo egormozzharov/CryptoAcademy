5a24169c3753f3a64facf2a9465d25d6980e4d64
egormozzharov <egormozzharov@gmail.com>
2022-06-02 02:01:42 +0300

    Solidity checkSignature method was implemented

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -5,19 +5,25 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/Utils/cryptography/ECDSA.sol";
 import "./interfaces/IERC721Mintable.sol";
 import "./interfaces/IERC1155Mintable.sol";
 import "./interfaces/IERC20.sol";
 
+using ECDSA for bytes32;
+
 contract Bridge {
-    mapping(string => bool) public signaturesWereProcessed;
+    mapping(bytes => bool) public signaturesWereProcessed;
 
     event SwapExecuted(string toNetwork, address toAddress, string fromNetwork, address fromAddress, address tokenContractAddress, uint amount);
+    event RedeemExecuted();
 
     string private _currentNetwork;
+    address immutable private _validatorAddress;
 
-    constructor(string memory currentNetwork) {
+    constructor(string memory currentNetwork, address validatorAddress) {
         _currentNetwork = currentNetwork;
+        _validatorAddress = validatorAddress;
     }
 
     function swap(string calldata toNetwork, address toAddress, address tokenContractAddress, uint amount) external {
@@ -25,29 +31,17 @@ contract Bridge {
         emit SwapExecuted(toNetwork, toAddress, _currentNetwork, msg.sender, tokenContractAddress, amount);
     }
 
-    function redeem(string calldata messageWithSignature, string calldata signature) external {
+    function redeem(bytes calldata signature, address tokenContractAddress, uint amount, uint nonce) external {
         require(signaturesWereProcessed[signature] == false, "Signature was already used");
-        require(verifySignature(messageWithSignature, signature), "Signature is not valid");
+        require(checkSignature(signature, tokenContractAddress, amount, nonce), "Signature is not valid");
         signaturesWereProcessed[signature] = true;
-        address tokenContractAddress = getTokenContractAddress(messageWithSignature);
-        address toAddress = getToAddress(messageWithSignature);
-        uint amount = getTokenAmount(messageWithSignature);
-        IERC20Mintable(tokenContractAddress).mint(toAddress, amount);
+        IERC20Mintable(tokenContractAddress).mint(msg.sender, amount);
+        emit RedeemExecuted();
     }
 
-    function verifySignature(string calldata messageWithSignature, string calldata signature) internal returns (bool) {
-        return true;
-    }
-
-    function getTokenContractAddress(string calldata messageWithSignature) internal returns (address) {
-        return address(0);
-    }
-
-    function getTokenAmount(string calldata messageWithSignature) internal returns (uint) {
-        return 0;
-    }
-
-    function getToAddress(string calldata messageWithSignature) internal returns (address) {
-        return address(0);
+    function checkSignature(bytes calldata signature, address tokenContractAddress, uint amount, uint nonce) internal view returns (bool) {
+        bytes memory data = abi.encode(tokenContractAddress, amount, nonce);
+        address signer = keccak256(data).toEthSignedMessageHash().recover(signature);
+        return signer == _validatorAddress;
     }
 }
