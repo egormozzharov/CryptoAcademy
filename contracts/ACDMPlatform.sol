ad9c658023d72afe8ece3013e4c58cf49c8c8a46
egormozzharov <egormozzharov@gmail.com>
2022-06-19 23:24:55 +0300

    Unit tests were created

--- contracts/ACDMPlatform.sol
+++ contracts/ACDMPlatform.sol
@@ -4,23 +4,6 @@ pragma solidity ^0.8.0;
 import "./interfaces/IERC20.sol";
 import "hardhat/console.sol";
 
-contract NextPriceGenerator {
-    uint private currentPrice;
-    bool private isFirstStep;
-    constructor(uint _initialPrice) {
-        currentPrice = _initialPrice;
-        isFirstStep = true;
-    }
-
-    function getNext() public returns (uint) {
-        if (isFirstStep) {
-            isFirstStep = false;
-            return currentPrice;
-        }
-        return currentPrice * 103 / 100 + 4 * 10**12;
-    }
-}
-
 contract ACDMPlatform {
     struct Order {
         bool isProcessed;
@@ -29,6 +12,7 @@ contract ACDMPlatform {
         uint pricePerUnit;
     }
 
+    bool public isFirstRound;
     bool public saleIsActive;
     bool public tradingIsActive;
     address public immutable acdmToken;
@@ -45,21 +29,20 @@ contract ACDMPlatform {
     event UserRegistered(address _newUser, address _referer);
     event SaleRoundStarted();
     event BuyACDM(address buyer, uint amount);
+    event BuyOrder(address buyer, uint amount);
 
     event TradeRoundStarted();
     event OrderAdded(uint _amount, uint _pricePerUnit, address owner);
     event OrderRemoved(uint orderId);
 
-    NextPriceGenerator public nextPriceGenerator;
-
     constructor(address _acdmToken, uint _roundTime) {
         require(_acdmToken != address(0), "ACDM token cannot be the zero address");
         acdmToken = _acdmToken;
         roundTime = _roundTime;
+        isFirstRound = true;
         tradingWeiAmount = 1 * 10**18;
-        pricePerUnitInCurrentPeriod = ((1 * 10**18) / (100000 * 10**6));
-        console.log(pricePerUnitInCurrentPeriod);
-        nextPriceGenerator = new NextPriceGenerator(pricePerUnitInCurrentPeriod);
+        amountInCurrentPeriod = 100000 * 10**6;
+        pricePerUnitInCurrentPeriod = (tradingWeiAmount / amountInCurrentPeriod);
     }
 
     function register(address _newUser, address _referer) external {
@@ -75,16 +58,35 @@ contract ACDMPlatform {
 
     function startSaleRound() external {
         require(block.timestamp >= tradingEndTime, "Tranding period is not ended yet");
+        pricePerUnitInCurrentPeriod = getNextPricePerUnitInCurrentPeriod();
+        amountInCurrentPeriod = getNextAmountOfACDM();
+        isFirstRound = false;
         tradingIsActive = false;
         saleIsActive = true;
-        pricePerUnitInCurrentPeriod = nextPriceGenerator.getNext();
-        amountInCurrentPeriod = tradingWeiAmount / pricePerUnitInCurrentPeriod;
         IERC20Mintable(acdmToken).mint(address(this), amountInCurrentPeriod);
         emit SaleRoundStarted();
     }
 
+    function getNextPricePerUnitInCurrentPeriod() private view returns(uint) {
+        if (isFirstRound) {
+            return pricePerUnitInCurrentPeriod;
+        }
+        return pricePerUnitInCurrentPeriod * 103 / 100 + 4 * 10**12;
+    }
+
+    function getNextAmountOfACDM() private view returns(uint) {
+        if (isFirstRound) {
+           return amountInCurrentPeriod;
+        }
+        return tradingWeiAmount / pricePerUnitInCurrentPeriod;
+    }
+
     function buyACDM() payable external {
+        console.log("msgvalue=", msg.value);
+        console.log("pricePerUnitInCurrentPeriod=", pricePerUnitInCurrentPeriod);
         uint amountToBuy = msg.value / pricePerUnitInCurrentPeriod;
+        console.log("amountToBuy=", amountToBuy);
+        console.log("amountInCurrentPeriod=", amountInCurrentPeriod);
         require (amountInCurrentPeriod > amountToBuy, "Order amount must be greater or equal to the sender amount");
         amountInCurrentPeriod = amountInCurrentPeriod - amountToBuy;
         IERC20(acdmToken).transfer(msg.sender, amountToBuy);
@@ -101,6 +103,8 @@ contract ACDMPlatform {
     }
 
     function addOrder(uint _amount, uint _pricePerUnit) external {
+        require(tradingIsActive == true, "Trading should be acvive");
+        IERC20(acdmToken).transferFrom(msg.sender, address(this), _amount);
         Order memory order = Order(
         {
             amount: _amount,
@@ -113,6 +117,8 @@ contract ACDMPlatform {
     }
 
     function removeOrder(uint _orderId) external {
+        Order storage order = orders[_orderId];
+        IERC20(acdmToken).transfer(order.owner, order.amount);
         delete orders[_orderId];
         emit OrderRemoved(_orderId);
     }
@@ -120,7 +126,8 @@ contract ACDMPlatform {
     function buyOrder(uint _orderId) payable external {
         Order memory order = orders[_orderId];
         uint amountToBuy = msg.value / order.pricePerUnit;
-        require (order.amount > amountToBuy, "Order amount must be greater or equal to the sender amount");
+        require (amountToBuy > 0, "Amount to buy must be greater that zero");
+        require (order.amount >= amountToBuy, "Order amount must be greater or equal to the sender amount");
         if (order.amount == amountToBuy) {
             order.isProcessed = true;
             order.amount = 0;
@@ -128,5 +135,7 @@ contract ACDMPlatform {
             order.amount = order.amount - amountToBuy;
         }
         tradingWeiAmount = tradingWeiAmount + msg.value;
+        IERC20(acdmToken).transfer(msg.sender, amountToBuy);
+        emit BuyOrder(msg.sender, amountToBuy);
     }
 }
--- test/ACDMPlatform.ts
+++ test/ACDMPlatform.ts
@@ -3,8 +3,8 @@ import { ethers } from "hardhat";
 import { ContractFactory } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { ACDMToken } from '../typechain-types/contracts/ACDMToken';
-import { ACDMPlatform } from '../typechain-types/contracts/ACDMPlatform.sol/ACDMPlatform';
 import { blockTimestampTools } from '../scripts/tools';
+import { ACDMPlatform } from '../typechain-types/contracts/ACDMPlatform';
 
 describe("ACDMPlatform", function () {
 
@@ -30,8 +30,9 @@ describe("ACDMPlatform", function () {
 
   describe("Initial values", function () {
     it("Initial price should be correct", async function () {
+      expect(await acdmPlatform.tradingWeiAmount()).to.be.equal(BigInt("1000000000000000000"));
       expect(await acdmPlatform.pricePerUnitInCurrentPeriod()).to.be.equal(10000000);
-      expect(await acdmPlatform.tradingWeiAmount()).equal(BigInt("1000000000000000000"));
+      expect(await acdmPlatform.amountInCurrentPeriod()).to.be.equal(BigInt("100000000000"));
       expect(await acdmPlatform.roundTime()).to.be.equal(roundInterval);
       expect(await acdmPlatform.acdmToken()).to.be.equal(acdmToken.address);
     });
@@ -54,17 +55,92 @@ describe("ACDMPlatform", function () {
 
   describe("startSaleRound", function () {
     it("Shoud start sale round successfully", async function () {
-      await expect(acdmPlatform.startSaleRound())
+      await expect(await acdmPlatform.startSaleRound())
         .to.emit(acdmPlatform, "SaleRoundStarted");
+      expect(await acdmPlatform.pricePerUnitInCurrentPeriod()).to.be.equal(10000000);
+      expect(await acdmPlatform.amountInCurrentPeriod()).to.be.equal(BigInt("100000000000"));
+      expect(await acdmPlatform.saleIsActive()).to.be.equal(true);
+      expect(await acdmPlatform.tradingIsActive()).to.be.equal(false);
+      expect(await acdmPlatform.isFirstRound()).to.be.equal(false);
     });
+  });
 
-    // it("Shoud fail widthdraw if you have active deposits", async function () {
-    //   await daoContract.connect(owner).addProposal("description", getExternalContractCallData(50), stakingContract.address);
-    //   await daoContract.connect(owner).deposit(100);
-    //   await daoContract.connect(owner).voteProposal(1, true);
+  describe("buyACDM", function () {
+    it("Shoud buy successfully", async function () {
+      await acdmPlatform.startSaleRound();
+      await expect(await acdmPlatform.connect(owner).buyACDM({ value: ethers.utils.parseEther("0.000001") }))
+        .to.emit(acdmPlatform, "BuyACDM").withArgs(owner.address, 100000);
+      await expect(await acdmPlatform.amountInCurrentPeriod()).to.be.equal(99999900000);
+    });
+  });
 
-    //   await expect(daoContract.connect(owner).widthdraw())
-    //     .to.be.revertedWith("You can only widthdraw when all your deposits debating periods has passed");
-    // });
+  describe("startTradeRound", function () {
+    it("Shoud start trade round successfully", async function () {
+      await acdmPlatform.startSaleRound();
+      await acdmPlatform.connect(owner).buyACDM({ value: ethers.utils.parseEther("0.000001") });
+      await blockTimestampTools.forwardTimestamp(roundInterval);
+
+      await expect(await acdmPlatform.startTradeRound())
+        .to.emit(acdmPlatform, "TradeRoundStarted");
+      await expect(await acdmPlatform.saleIsActive()).to.be.equal(false);
+      await expect(await acdmPlatform.tradingIsActive()).to.be.equal(true);
+      await expect(await acdmPlatform.tradingWeiAmount()).to.be.equal(0);
+    });
+  });
+
+  describe("startTradeRound", function () {
+    it("Shoud start trade round successfully", async function () {
+      await acdmPlatform.startSaleRound();
+      await acdmPlatform.connect(owner).buyACDM({ value: ethers.utils.parseEther("0.000001") });
+      await blockTimestampTools.forwardTimestamp(roundInterval);
+
+      await expect(await acdmPlatform.startTradeRound())
+        .to.emit(acdmPlatform, "TradeRoundStarted");
+      await expect(await acdmPlatform.saleIsActive()).to.be.equal(false);
+      await expect(await acdmPlatform.tradingIsActive()).to.be.equal(true);
+      await expect(await acdmPlatform.tradingWeiAmount()).to.be.equal(0);
+    });
+  });
+
+  describe("addOrder", function () {
+    it("Shoud add order successfully", async function () {
+      const amount = 1;
+      await acdmPlatform.startSaleRound();
+      await blockTimestampTools.forwardTimestamp(roundInterval);
+      await acdmPlatform.startTradeRound();
+      await acdmToken.approve(acdmPlatform.address, amount);
+      await expect(await acdmPlatform.connect(owner).addOrder(amount, 10000000000))
+        .to.emit(acdmPlatform, "OrderAdded").withArgs(amount, 10000000000, owner.address);
+      await expect(await acdmToken.balanceOf(acdmPlatform.address)).to.be.equal(amount);
+    });
+  });
+
+  describe("removeOrder", function () {
+    it("Shoud remove order successfully", async function () {
+      const amount = 1;
+      await acdmPlatform.startSaleRound();
+      await blockTimestampTools.forwardTimestamp(roundInterval);
+      await acdmPlatform.startTradeRound();
+      await acdmToken.approve(acdmPlatform.address, amount);
+      await acdmPlatform.connect(owner).addOrder(amount, 10000000000);
+      await expect(await acdmPlatform.connect(owner).removeOrder(0))
+        .to.emit(acdmPlatform, "OrderRemoved").withArgs(0);
+      await expect(await acdmToken.balanceOf(acdmPlatform.address)).to.be.equal(0);
+    });
+  });
+
+  describe("buyOrder", function () {
+    it("Shoud buy order successfully", async function () {
+      const amount = 1;
+      await acdmPlatform.startSaleRound();
+      await blockTimestampTools.forwardTimestamp(roundInterval);
+      await acdmPlatform.connect(owner).buyACDM({ value: ethers.utils.parseEther("0.000001") });
+      await acdmPlatform.startTradeRound();
+      await acdmToken.approve(acdmPlatform.address, amount);
+      await acdmPlatform.connect(owner).addOrder(amount, 10000000000);
+      await expect(await acdmPlatform.connect(addr1).buyOrder(0, {value: 10000000000}))
+        .to.emit(acdmPlatform, "BuyOrder").withArgs(addr1.address, amount);
+      await expect(await acdmToken.balanceOf(addr1.address)).to.be.equal(amount);
+    });
   });
 });
