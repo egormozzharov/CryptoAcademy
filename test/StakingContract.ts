447b2a03fd404fe5092b14aeeb60d3b1f4e2b9e9
egormozzharov <egormozzharov@gmail.com>
2022-06-23 11:15:45 +0300

    StakingContract tests were created

--- test/StakingContract.ts
+++ test/StakingContract.ts
@@ -9,24 +9,25 @@ import { IUniswapV2Factory } from '../typechain-types/contracts/interfaces/IUnis
 import { blockTimestampTools } from "../scripts/tools";
 import { XXXToken } from '../typechain-types/contracts/XXXToken';
 import { DAO } from '../typechain-types/contracts/DAO';
+import { ACDMPlatform } from '../typechain-types/contracts/ACDMPlatform';
+import { ACDMToken } from '../typechain-types/contracts/ACDMToken';
 
 describe("StakingContract", function () {
 
+  let daoContract: DAO;
+  let stakingContract: StakingContract;
+  let acdmPlatform: ACDMPlatform;
   let owner: SignerWithAddress;
   let addr1: SignerWithAddress;
   let addr2: SignerWithAddress;
-
-  let customToken: XXXToken;
-  let stakingContract: StakingContract;
-  let daoContract: DAO;
   let lpToken: IERC20;
+  let acdmToken: ACDMToken;
 
   const UniswapV2Router02Address: string = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
   const UniswapV2FactoryAddress: string = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 
   const ethAmount = ethers.BigNumber.from('1000000000');
   const customTokenAmount = ethers.BigNumber.from('10');
-
   const deadline = Math.floor(Date.now() / 1000) + 3600;
 
   const deployContract = async (contractName: string, ...args: any[]): Promise<Contract> => {
@@ -37,8 +38,11 @@ describe("StakingContract", function () {
 
   beforeEach(async function () {
     [owner, addr1, addr2] = await ethers.getSigners();
-    customToken = (await deployContract("XXXToken", 10000 * 1000000)) as XXXToken;
+
+    // Uniswap
+    const customToken = (await deployContract("XXXToken", 10000 * 1000000)) as XXXToken;
     customToken.connect(owner).approve(UniswapV2Router02Address, 1000);
+
     const router: IUniswapV2Router02 = (await ethers.getContractAt("IUniswapV2Router02", UniswapV2Router02Address)) as IUniswapV2Router02;
     await router.addLiquidityETH(
       customToken.address,
@@ -55,6 +59,13 @@ describe("StakingContract", function () {
     lpToken = (await ethers.getContractAt("IERC20", lpTokenAddress)) as IERC20;
     const lpTokenBalance = await lpToken.connect(owner).balanceOf(owner.address);
 
+    // Staking
+    stakingContract = (await deployContract("StakingContract", lpTokenAddress, customToken.address, 20, 3600)) as StakingContract;
+    await lpToken.connect(owner).approve(stakingContract.address, lpTokenBalance);
+    await lpToken.connect(owner).transfer(addr1.address, 100);
+    await lpToken.connect(addr1).approve(stakingContract.address, 100);
+
+    // DAO
     let chairPerson = owner.address;
     let minimumQuorum = 10;
     let debatingPeriod = 3600;
@@ -62,12 +73,21 @@ describe("StakingContract", function () {
     daoContract = (await contractFactory.connect(owner).deploy(chairPerson, minimumQuorum, debatingPeriod)) as DAO;
     await daoContract.deployed();
 
-    stakingContract = (await deployContract("StakingContract", lpTokenAddress, customToken.address, 20, 3600)) as StakingContract;
-    lpToken.connect(owner).approve(stakingContract.address, lpTokenBalance);
+    // ACDMPlatform
+    const adcmTokenSypply = 1000 * 10^6;
+    const acdmTokenContractFactory: ContractFactory = await ethers.getContractFactory("ACDMToken");
+    acdmToken = (await acdmTokenContractFactory.connect(owner).deploy(adcmTokenSypply)) as ACDMToken;
+    await acdmToken.deployed();
 
+    let roundInterval = 3600;
+    const acdmPlatformContractFactory: ContractFactory = await ethers.getContractFactory("ACDMPlatform");
+    acdmPlatform = (await acdmPlatformContractFactory.connect(owner).deploy(acdmToken.address, roundInterval)) as ACDMPlatform;
+    await acdmToken.deployed();
+
+    // Common
     await stakingContract.setDao(daoContract.address);
     await daoContract.setStaking(stakingContract.address);
-  });
+   });
 
   describe("stake", async function () {
     it("Should revert if already have a stake", async function () {
@@ -83,6 +103,13 @@ describe("StakingContract", function () {
   });
 
   describe("unstake", function () {
+    it("Shoud revert when tokens are locked by an acrive DAO proposal", async function () {
+      await daoContract.connect(owner).addProposal("Description", getExternalContractCallData(50), acdmPlatform.address);
+      await stakingContract.connect(owner).stake(100);
+      await daoContract.connect(owner).voteProposal(1, true);
+      await expect(stakingContract.connect(owner).unstake()).to.be.revertedWith("Tokens are only available after dao proposals intervals has elapsed");
+    });
+
     it("Shoud revert when timestamp is greater that reward time", async function () {
       await stakingContract.connect(owner).stake(100);
       await expect(stakingContract.connect(owner).unstake()).to.be.revertedWith("Tokens are only available after correct time period has elapsed");
@@ -125,4 +152,10 @@ describe("StakingContract", function () {
       .to.emit(stakingContract, "RewardsClaimed").withArgs(owner.address, 40);
     });
   });
+
+  function getExternalContractCallData(amount: number) {
+    let jsonAbi = [{ "constant": false, "inputs": [{ "name": "_rewardFraction", "type": "uint256" }], "name": "setRewardFractionForSaleRef1", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }];
+    let iFace = new ethers.utils.Interface(jsonAbi);
+    return iFace.encodeFunctionData('setRewardFractionForSaleRef1', [amount]);
+  }
 });
